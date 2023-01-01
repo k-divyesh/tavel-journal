754962fd750bac3e544b8a44a10b5c9052f01d3c
kdivyesh-cs <kdivyesh.cs@gmail.com>
2023-01-01 19:07:10 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar';
+import Card from './Components/Card';
+import data from './Data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const cards = data.map((item, i) => {
+    //     <Card key={i} {...item} />;
+    // });
+
+    return (
+        <>
+            <NavBar />
+            <main>{/* {cards} */}</main>
+        </>
+    );
 }
 
 export default App;
